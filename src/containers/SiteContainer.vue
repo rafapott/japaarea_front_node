e439485ec639b9bc095b51e88437a723bcf70ce9
rafael-adekz <45203454+rafael-adekz@users.noreply.github.com>
2020-09-28 16:48:21 -0300

    Primeiro commit
    Primerio commit

---
+++ src/containers/SiteContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="c-app">
+    
+    <router-view :key="$route.path"></router-view>
+            
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'SiteContainer',
+    data(){
+        return {};
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/site/Test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+    teste: {{listData}}
+
+  </div>
+</template>
+
+<script>
+
+const axios = require('axios');
+
+export default {
+  name: 'Test',
+  data () {
+    return {
+      backend_dir:"http://localhost:3000/",
+      listData:[]
+    }
+  },
+  async mounted(){
+    let data = await this.teste();
+
+    console.log('really data',data);
+    this.$set(this,'listData',data);
+  },
+  methods: {
+    async teste(){
+
+      let data = await axios.get(this.backend_dir+'users/getAll');
+
+      if(data.data){
+        if(data.data.ok){
+          return data.data.data;
+        }
+      }
+    }
+  }
+}
+
+</script>
